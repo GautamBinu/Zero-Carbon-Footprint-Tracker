18ef87a5baaeae5c09fef81c74b7a961bf282f7b
Dhruv <dm9334@rit.edu>
2026-02-24 09:10:30 +0400

    EnergyEmissionn

--- carbonfootprint/EnergyEmission.java
+++ carbonfootprint/EnergyEmission.java
@@ -1,52 +1,53 @@
 public class EnergyEmission extends EmissionSource{
-    private Double kWHused;
+    private double kWHused;
     private String energySource;
     
-    public EnergyEmission(String sourceID, String category, String date, String userName,Double kWHused, String energySource){
+    public EnergyEmission(String sourceID, String category, String date, String userName,double  kWHused, String energySource){
         super(sourceID, category, date, userName);
         this.kWHused=kWHused;
         this.energySource=energySource;
 
     }
 
-    public Double kWHused(){
+    public double  getKwhused(){
         return this.kWHused;
     }
-    public void setkWHused(Double kWHused){
+    public void setKwhused(double  kWHused){
         this.kWHused=kWHused;
     }
-    public String energySource(){
+    public String getEnergySource(){
         return this.energySource;
     }
-    public void energySource(String energySource){
+    public void setEnergySource(String energySource){
         this.energySource=energySource;
     }
 
     @Override
     public double calculateEmission(){
-        double energyFactor=1.0;
+
+        if (kWHused == null || energySource == null) {
+            return 0.0;
+        }
+        double  energyFactor=0.0;
         energySource=energySource.toLowerCase();
 
-        if (energySource.equals("Grid")){
+        if (energySource.equals("grid")){
             energyFactor=2.0;
 
         }
-        else if (energySource.equals(("Solar"))){
+        else if (energySource.equals("solar")){
             energyFactor=0.65;
 
         }
-        else if (energySource.equals("Wind")){
+        else if (energySource.equals("wind")){
             energyFactor=0.75;
-        }
-        else{
-            return energyFactor;
         }
         return kWHused*energyFactor;
     }
 
     @Override
     public String toString(){
-        return super.toString() + "Energy Source"+energySource+"kWH used"+kWHused;
+        return super.toString() + "Energy Source: "+energySource+"kWH used: "+kWHused;
     }
             
     
