49f30df0b0cc3be8145c6d7ae4e949199768a15a
Gautam Binu <75609533+GautamBinu@users.noreply.github.com>
2026-03-21 18:12:10 +0400

    Refactor GreenPrintCLI by removing start method
    Removed the implementation of the start method and the tracker instance.

--- GreenPrintCLI.java
+++ GreenPrintCLI.java
@@ -9,75 +9,12 @@ import javafx.geometry.Side;
 
 public class GreenPrintCLI extends Application {
 
-    static FootprintTracker tracker = new FootprintTracker();
-    static Tab dashboardTab;
-
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("GreenPrint CLI");
-
-        // Save state when window closes
-        primaryStage.setOnCloseRequest(event -> {
-            Logger.saveState();
-        });
-
-        // Create the TabPane
-        TabPane tabPane = new TabPane();
-        
-        tabPane.setStyle( 
-        "-fx-font-family: 'Segoe UI', Helvetica, Arial, sans-serif; " +
-        "-fx-font-size: 13.5px; " +
-        "-fx-font-weight: bold;" 
-          
-        );      
-        
-        
-        tabPane.setTabMinHeight(40);
-        tabPane.setTabMinWidth(150);
-       
-        tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
-        tabPane.setSide(Side.BOTTOM);
-
-        // Create the Dashboard tab
-        dashboardTab = new Tab();
-        dashboardTab.setText("Dashboard");
-        dashboardTab.setContent(Dashboard.createDashboard());
-        
-        // Create the Data Operations tab
-        Tab dataOpsTab = new Tab();
-        dataOpsTab.setText("Data Operations");
-        dataOpsTab.setContent(DataOperationIO.DataOperationsTab());
-        
-        
-        // Create the Offset Transactions tab
-        Tab offsetTab = new Tab();
-        offsetTab.setText("Offset Transactions");
-        offsetTab.setContent(OffsetTransactionGUI.createOffsetTabPane());
-        
-        // Add tabs to the TabPane
-        tabPane.getTabs().addAll(dashboardTab, dataOpsTab, offsetTab);
-        
-        // Create the scene and add the TabPane
-        Scene scene = new Scene(tabPane, 800, 600);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    public static void refreshDashboard() {
-        if (dashboardTab != null) {
-            dashboardTab.setContent(Dashboard.createDashboard());
-        }
-       
-    }
+    
+ 
 
     public static void main(String[] args) throws IOException {
 
-        // Load saved state on startup
-        Logger.loadState();
-
         
-        // Launch the JavaFX application
-        launch(args);
     }
 }
 
